bf0aed30b978b1208cf56c2e606d1c50b3ecb32a
Mr-reinert <reinert.neto@gmail.com>
2022-09-17 01:22:00 -0300

    main

--- analise_metas_premio/main.py
+++ analise_metas_premio/main.py
@@ -1,16 +1,31 @@
-# This is a sample Python script.
+import pandas as pd
+from twilio.rest import Client
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Inserir código de conta criada no twilio para poder enviar as notificações por SMS
+account_sid = "AXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+# Inserir Token
+auth_token  = "AXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+client = Client(account_sid, auth_token)
 
+#define vetor único para comportar os meses de suas respectivas tabelas
+lista_meses = ['janeiro', 'fevereiro','março','abril','maio','junho']
+meta = 50000
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+#leitura em todas as tabelas com laço de repetição
+for i in lista_meses:
+    tabela_vendas = pd.read_excel(f'{i}.xlsx')
 
+#se na tabela 'Vendas' se existe algum número maior que 55.000 então...
+    if (tabela_vendas['Vendas'] > meta).any():
+        #localizará na tabela (linha/coluna) desejada, e quantos valores{ .loc[(linha),'coluna'].values[0] }
+        vendedor = tabela_vendas.loc[(tabela_vendas['Vendas'] > meta),'Vendedor'].values[0]
+        vendas = tabela_vendas.loc[tabela_vendas['Vendas'] > meta,'Vendas'].values[0]
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+        #envia mensagem com base no seu cadastro no twilio
+        message = client.messages.create(
+            to="+550000000000",
+            from_="+123456789",
+            body=f"Metas batidas no mês de {i}:\n Vendedor: {vendedor}\n Vendas: R$ {vendas}\n")
+        print(message.sid)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        print(f"Metas batidas no mês de {i}:\n Vendedor: {vendedor}\n Vendas: R$ {vendas}\n")
